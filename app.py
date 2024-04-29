b677a08019cee664a49ed9a0a6b596fdea19f013
shanef97 <165854100+shanef97@users.noreply.github.com>
2024-04-28 21:01:48 -0400

    Fixed app.py

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask
-from views import views
+from flask import Flask, Blueprint, render_template, redirect, url_for, request
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/views")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method=="POST":
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        with open('feedback.txt', 'a') as f:
+            f.write(f'Name: {name}, Email: {email}, Message: {message}\n')
+        return render_template('thankyou.html')
+    else:
+        return render_template("contact.html")
+
+@app.route("/reviews")
+def reviews():
+    return render_template("reviews.html")
+
--- views.py
+++ views.py
@@ -2,30 +2,3 @@ from flask import Blueprint, render_template, redirect, url_for, request
 
 views = Blueprint(__name__, "views")
 
-@views.route("/")
-def home():
-    return render_template("index.html")
-
-@views.route("/gohome")
-def go_home():
-    return redirect(url_for("views.home"))
-
-@views.route("/about")
-def about():
-    return render_template("about.html")
-
-@views.route("/contact", methods=["GET", "POST"])
-def contact():
-    if request.method=="POST":
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
-        with open('feedback.txt', 'a') as f:
-            f.write(f'Name: {name}, Email: {email}, Message: {message}\n')
-        return render_template('thankyou.html')
-    else:
-        return render_template("contact.html")
-
-@views.route("/reviews")
-def reviews():
-    return render_template("reviews.html")
