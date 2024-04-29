151bea02dda92a8f6c939046be2feebbea46db4f
shanef97 <165854100+shanef97@users.noreply.github.com>
2024-04-29 17:32:22 -0400

    Scrapped email functionality

--- views.py
+++ views.py
@@ -1,19 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request
-from app import app
-from flask_mail import Mail, Message
 
 views = Blueprint(__name__, "views")
-
-mail = Mail(app)
-
-app.config.update(dict(
-    MAIL_SERVER = 'smtp.gmail.com',
-    MAIL_PORT = 465,
-    MAIL_USE_TLS = False,
-    MAIL_USE_SSL = True,
-    MAIL_USERNAME = 'ferrellshane470@gmail.com',
-    MAIL_PASSWORD = "b8wja'tQ13.0"
-))
 
 @views.route("/")
 def home():
@@ -39,9 +26,6 @@ def contact():
         name = request.form.get['name']
         email = request.form.get['email']
         message = request.form.get['message']
-        msg = Message('Test', sender='mymail@mail.com', recipients=['mymai@mail.com'])
-        msg.body = "Contact form submitted with data:\n\nName: {}\n\nE-mail: {}\n\nMessage: {}".format(name, email, message)
-        mail.send(msg)
 
         return render_template('thankyou.html')
     else:
