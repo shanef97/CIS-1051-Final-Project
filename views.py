73998af39a4a53fb0aaacddf8e098844734022c6
shanef97 <165854100+shanef97@users.noreply.github.com>
2024-04-28 22:00:03 -0400

    readded contact page

--- views.py
+++ views.py
@@ -14,3 +14,15 @@ def about():
 @views.route("/reviews")
 def reviews():
     return render_template("reviews.html")
+
+@views.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method=="POST":
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        with open('feedback.txt', 'a') as f:
+            f.write(f'Name: {name}, Email: {email}, Message: {message}\n')
+        return render_template('thankyou.html')
+    else:
+        return render_template("contact.html")
