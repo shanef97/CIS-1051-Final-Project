488da93a1e7b3a7eb397940c5067032fce4b8ec5
shanef97 <165854100+shanef97@users.noreply.github.com>
2024-04-28 21:38:45 -0400

    fixed app and views

--- app.py
+++ app.py
@@ -1,32 +1,11 @@
 from flask import Flask
-from flask import Blueprint, render_template, redirect, url_for, request
+from views import views
 
 app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact", methods=["GET", "POST"])
-def contact():
-    if request.method=="POST":
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
-        with open('feedback.txt', 'a') as f:
-            f.write(f'Name: {name}, Email: {email}, Message: {message}\n')
-        return render_template('thankyou.html')
-    else:
-        return render_template("contact.html")
-
-@app.route("/reviews")
-def reviews():
-    return render_template("reviews.html")
 
